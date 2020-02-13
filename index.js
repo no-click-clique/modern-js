f43af3e836d7a98011ea89394b12910f7cfa3a96
dani <dani.zaghian@gmail.com>
2020-02-13 09:47:21 -0800

    adds node modules, package.json, moment library

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var message = "Hello from JS";
 console.log(message);
-// document.querySelector('p').innerHTML = message;
+// document.querySelector("p").innerHTML = message;
+var startOfToday = moment().startOf('day').fromNow();
+console.log(startOfToday);

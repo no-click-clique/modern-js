d41268c3389b09dedf8f9a65a8910d5098975a1b
dani <dani.zaghian@gmail.com>
2020-02-13 10:46:56 -0800

    adds webpack, require syntax, mathjs dependency

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 var message = "Hello from JS";
 console.log(message);
 // document.querySelector("p").innerHTML = message;
 var startOfToday = moment().startOf('day').fromNow();
 console.log(startOfToday);
+
+console.log(math.log(10000, 10));

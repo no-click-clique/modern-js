ce8a1e0d0ad74ac7737a7b71e7782fcad926bbdb
dani <dani.zaghian@gmail.com>
2020-02-13 11:44:25 -0800

    adds babel, es2015 syntax

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var moment = require('moment');
+import moment from "moment";
 var math = require('mathjs');
 
 var message = "Hello from JS";
 console.log(message);
 // document.querySelector("p").innerHTML = message;
 var startOfToday = moment().startOf('day').fromNow();
-console.log(startOfToday);
+console.log(`The start of today was ${startOfToday}`);
 
 console.log(math.log(10000, 10));

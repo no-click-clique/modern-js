13acfd6b7e552ed230729f708d3820042e1ae658
dani <dani.zaghian@gmail.com>
2020-02-13 12:09:14 -0800

    adds webpack dev server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var message = "Hello from JS";
 console.log(message);
 // document.querySelector("p").innerHTML = message;
 var startOfToday = moment().startOf('day').fromNow();
-console.log(`The start of today was ${startOfToday}`);
+
+console.log(`The start of today was ${startOfToday}!`);
 
 console.log(math.log(10000, 10));
